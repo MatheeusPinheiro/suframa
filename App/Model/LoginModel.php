2098c56fd1c94ad0dd06f38b4aca15623e9a8c9e
Matheus Pinheiro <78563956+MatheeusPinheiro@users.noreply.github.com>
2022-09-24 19:02:13 -0400

    primeiro commit

---
+++ App/Model/LoginModel.php
@@ -0,0 +1,43 @@
+<?php
+
+class LoginModel
+{
+    public $id;
+    public $nome;
+    public $login;
+    public $senha;
+    public $tipouser;
+    private $conexao;
+
+    public function __construct()
+	{
+		$dsn = "mysql:host=localhost:3306;dbname=suframa";
+
+		$this->conexao = new PDO($dsn, 'root', '1234');
+	}
+
+    public function validarLogin(string $login, string $senha, string $tipouser)
+    {
+        include 'DAO/LoginDAO.php ';
+        $dao = new LoginDAO();
+
+        $obj = $dao->validarUser($login, $senha, $tipouser);
+        if($obj){
+            return $obj;
+        }else{
+            return false;
+        }
+    }
+
+	public function getByLogin(string $login)
+    {
+        include 'DAO/LoginDAO.php ';
+        $dao = new LoginDAO();
+
+        $obj = $dao->selectByLogin($login);
+        if($obj){
+            return $obj;
+        }
+    }
+
+}
